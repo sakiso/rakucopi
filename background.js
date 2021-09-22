c72e347c86f88d0704f81c6a37f5e251a1ac65cd
sohei yamasaki <>
2021-09-22 22:44:52 +0900

    やっぱonInstalled戻す

--- background.js
+++ background.js
@@ -1,18 +1,18 @@
 'use strict';
 
 {
-  //   chrome.runtime.onInstalled.addListener(() => {
-  //     //拡張機能のインストール・VerUPの際に呼ばれる。ブラウザを閉じても残る。
-  const parent = chrome.contextMenus.create({
-    id: 'copy_mdurl',
-    title: 'URLコピー(Markdown)',
-    contexts: ['page', 'selection'],
+  chrome.runtime.onInstalled.addListener(() => {
+    //拡張機能のインストール・VerUPの際に呼ばれる。ブラウザを閉じても残る。
+    const parent = chrome.contextMenus.create({
+      id: 'copy_mdurl',
+      title: 'URLコピー(Markdown)',
+      contexts: ['all'],
+    });
   });
-  //   });
 
   // メニューをクリック時に実行
   chrome.contextMenus.onClicked.addListener((item) => {
-    const wk = info.selectionText;
-    console.log(wk);
+    // const wk = info.selectionText;
+    console.log('hello2');
   });
 }
