85ae708b1c133af83b597c7a66b6b862763cd415
sohei yamasaki <>
2021-09-22 21:52:23 +0900

    onInstalled消してみる

--- event.js
+++ event.js
@@ -1,14 +1,14 @@
 'use strict';
 
 {
-  chrome.runtime.onInstalled.addListener(() => {
-    //拡張機能のインストール・VerUPの際に呼ばれる。ブラウザを閉じても残る。
-    const parent = chrome.contextMenus.create({
-      id: 'copy_mdurl',
-      title: 'URLコピー(Markdown)',
-      contexts: ['page', 'selection'],
-    });
+  //   chrome.runtime.onInstalled.addListener(() => {
+  //     //拡張機能のインストール・VerUPの際に呼ばれる。ブラウザを閉じても残る。
+  const parent = chrome.contextMenus.create({
+    id: 'copy_mdurl',
+    title: 'URLコピー(Markdown)',
+    contexts: ['page', 'selection'],
   });
+  //   });
 
   // メニューをクリック時に実行
   chrome.contextMenus.onClicked.addListener((item) => {
