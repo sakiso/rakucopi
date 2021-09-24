74dafd8175d3d7d9808edb037369cde182d169c9
sohei yamasaki <>
2021-09-24 09:33:09 +0900

    同期処理がうまくいかない(contentのレスポンスを待たずバックグラウンド側が進んでいってしまう)

--- content.js
+++ content.js
@@ -1,8 +1,17 @@
-chrome.runtime.onMessage.addListener((message, sender) => {
-  navigator.clipboard
-    .writeText(message.content)
-    .then()
-    .catch((err) => {
-      console.log(err)
-    })
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  switch (message.type) {
+    case 'write to clipboard':
+      console.log('write clip')
+      navigator.clipboard
+        .writeText(message.content)
+        .then()
+        .catch((err) => {
+          console.log(err)
+        })
+      break
+    case 'get selected text include newline-code':
+      console.log('content_quote')
+      return Promise.resolve({ response: 'quote success!' })
+      break
+  }
 })
